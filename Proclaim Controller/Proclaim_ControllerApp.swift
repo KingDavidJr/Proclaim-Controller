132e30e5fc3e93d72c44269d472c21bc996268ea
David <realkd872@gmail.com>
2023-03-05 11:37:31 -0600

    Initial Commit

---
+++ Proclaim Controller/Proclaim_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proclaim_ControllerApp.swift
+//  Proclaim Controller
+//
+//  Created by David Amedeka on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Proclaim_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
